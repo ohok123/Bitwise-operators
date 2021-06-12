7fbd5c334408f9d01833620fcd1c75c8585cc7f2
ohok123 <57207141+ohok123@users.noreply.github.com>
2021-06-11 19:36:20 -0500

    Add files via upload

---
+++ major1.h
@@ -0,0 +1,27 @@
+/*
+Team members:           Luis Roman, Arturo Roman, Yicheng Feng, Nicholas Norado
+Course Section:         001
+Date:                   9/25/2019
+Description:            These are the 4 functions that are used in major1.c
+
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+void EndianSwap(unsigned int input1);
+
+
+
+void parityFunction(unsigned int input);
+
+
+
+void clz(unsigned int input1);
+
+
+
+void Rotate(unsigned int input1,unsigned int input2);
